483e5dba03e61d7782f722fbf9ef9cc4517a5888
Sudharshan Reddy Chennupalle <sudharshanreddy25062002@gmail.com>
2025-02-19 22:29:53 +0530

    updated pom.xml file and created new package

---
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/g1/CDIContextLauncherApplication.java
@@ -0,0 +1,25 @@
+package com.in28minutes.learn_spring_framework.examples.g1;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan("com.in28minutes.learn_spring_framework.examples.g1")
+public class CDIContextLauncherApplication {
+	
+
+	public static void main(String[] args) {
+		
+		try(var context = new AnnotationConfigApplicationContext(CDIContextLauncherApplication.class);){
+			
+			Arrays.stream(context.getBeanDefinitionNames())
+				.forEach(System.out::println);
+		}
+
+	}
+
+}
