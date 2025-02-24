ea4fb0b42f23828e7819a2cc7b39591050c7ffe3
Sudharshan Reddy Chennupalle <sudharshanreddy25062002@gmail.com>
2025-02-24 19:27:37 +0530

    Able to create a bean and get it value

--- learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/h1/XmlConfigurationContextLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/h1/XmlConfigurationContextLauncherApplication.java
@@ -10,10 +10,12 @@ public class XmlConfigurationContextLauncherApplication {
 
 	public static void main(String[] args) {
 		
-		try(var context = new ClassPathXmlApplicationContext("contextConfiguration");){
+		try(var context = new ClassPathXmlApplicationContext("contextConfiguration.xml")){
 			
 			Arrays.stream(context.getBeanDefinitionNames())
 				.forEach(System.out::println);
+			
+			System.out.println(context.getBean("name"));
 		}
 
 	}
