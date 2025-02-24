a2536f415733215a12c753c597b8660368ffb2be
Sudharshan Reddy Chennupalle <sudharshanreddy25062002@gmail.com>
2025-02-24 19:20:02 +0530

    Running using Xml Configuration

--- learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/h1/XmlConfigurationContextLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/h1/XmlConfigurationContextLauncherApplication.java
@@ -2,19 +2,15 @@ package com.in28minutes.learn_spring_framework.examples.h1;
 
 import java.util.Arrays;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
-@Configuration
-@ComponentScan("com.in28minutes.learn_spring_framework.examples.h1")
 public class XmlConfigurationContextLauncherApplication {
 	
 
 	public static void main(String[] args) {
 		
-		try(var context = new AnnotationConfigApplicationContext(XmlConfigurationContextLauncherApplication.class);){
+		try(var context = new ClassPathXmlApplicationContext("contextConfiguration");){
 			
 			Arrays.stream(context.getBeanDefinitionNames())
 				.forEach(System.out::println);
